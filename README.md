3406a1c96562168a3bf9bbcaaf65441ee83b6388
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-05-24 16:00:32 +0700

    Component Properties

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ cdnjs.com:
 
 1. babel-core: 'https://cdnjs.com/libraries/babel-core'
 2. react: 'https://cdnjs.com/libraries/react'
+
+documentasi:
+
+1. babel jsx (repl) : 'https://babeljs.io/repl/'
--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,28 @@
 var Greeter = React.createClass({
+
+        //ini dibuat untuk default bila namenyan tidak ditulis di reactDOm
+        getDefaultProps: function(){
+                return{
+                        name: 'React',
+                        message: 'This is default message'
+                }
+        },
     render: function(){
+            var name = this.props.name;
+            var message = this.props.message;
         return(
           <div>
-            <h1>Hello React JSX component!</h1>
-            <p>this is form component</p>
+            <h1>Hello {name}</h1>
+            <p>{message + '!!'}</p>
           </div>
             //tidak bisa bila ada 2 div dalam 1 component
            )
         }
 });
-
+var firstName = 'Faza'
 
 ReactDOM.render(
-        <Greeter/>, 
+        // <Greeter name= {firstName} message='message is from props'/>, 
+        <Greeter name= {firstName}/>,
         document.getElementById('app') 
         );
